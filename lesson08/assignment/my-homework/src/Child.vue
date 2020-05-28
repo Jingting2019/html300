e07be69024383f93dcd12058374aeb8a285ce1ad
Jingting <fengshahuang@gmail.com>
2020-05-27 22:47:02 -0700

    used component, minxin, filter, and slots

---
+++ lesson08/assignment/my-homework/src/Child.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'child',
+  props: ['child'],
+}
+
+</script>
+
+
+
+
+Vue.component('child', {
+props: ['text'],
+template: `<p>{{ text }}</p>`
+});
---
+++ lesson08/assignment/my-homework/src/views/Recovery.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="history">
+    <Header></Header>
+    <h1 slot="page">This is about Recovery</h1>
+    <p>There randomly shows a similar project about desert recovery. Fresh page to see if there is any difference.</p>
+<!-- Randomly show one similar project about this topic -->
+    <div v-if="Math.random() > 0.5">
+      <img src="/assets/KubuqiNow.png" alt="Kubuqi is green Now">
+    </div>
+    <div v-else>
+      <img src="/assets/saihanbaNow.png" alt="saihanba is a green forest Now">
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from '../components/Header.vue';
+
+  export default {
+    components: {
+      Header
+    },
+  }
+</script>
